52894b0a76c515c579f78d1c2c3da1ac680e76a4
Matthew Stoodley <matt.storyfeet@gmail.com>
2026-08-14 21:09:46 +0100

    Got Database into shared async state

--- src/server.rs
+++ src/server.rs
@@ -1,18 +1,37 @@
 use anyhow::*;
 use axum::{Router, routing::get};
+use diesel::sqlite::SqliteConnection;
+use diesel_async::{
+    pooled_connection::{AsyncDieselConnectionManager, bb8::Pool},
+    sync_connection_wrapper::SyncConnectionWrapper,
+};
+
 use err_tools::{traceable::*, *};
 
 async fn hello() -> &'static str {
     "hello fools"
 }
 
+type CManager = SyncConnectionWrapper<SqliteConnection>;
+
 pub fn run_server() -> Result<(), TraceError> {
     println!("Running Server");
 
     let rt = tokio::runtime::Runtime::new().map_err(any_wrap!("Could not start runtime"))?;
 
+    let db_url =
+        dotenvy::var("DATABASE_URL").map_err(any_wrap!(".env DATABASE_URL not provided"))?;
+
     rt.block_on(async {
-        let app = Router::new().route("/", get(hello));
+        //let connection = SyncConnectionWrapper::<SqliteConnection>::establish(&db_url).await.expect("Could not establish connection");
+
+        let manager = AsyncDieselConnectionManager::<CManager>::new(&db_url);
+        let pool: Pool<CManager> = Pool::builder()
+            .build(manager)
+            .await
+            .expect("Could not build connection pool");
+
+        let app = Router::new().route("/", get(hello)).with_state(pool);
 
         // run our app with hyper, listening globally on port 3000
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
